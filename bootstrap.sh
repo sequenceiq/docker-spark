5fdb3f2579c911393fc723f13d0e7e3a6805d7f7
Aaron Steers <asteers@arivale.com>
2017-04-18 16:00:57 -0700

    wip

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,37 +1,32 @@
 #!/bin/bash
 
-: ${HADOOP_HOME:=/usr/local/hadoop}
-
-$HADOOP_CONF_DIR/hadoop-env.sh
-
-rm /tmp/*.pid
-
 # installing libraries if any - (resource urls added comma separated to the ACP system variable)
 cd $HADOOP_HOME/share/hadoop/common ; for cp in ${ACP//,/ }; do  echo == $cp; curl -LO $cp ; done; cd -
 
-# altering the core-site configuration
+# customizing core-site.xml configuration
 sed s/HOSTNAME/$HOSTNAME/ $HADOOP_CONF_DIR/core-site.xml.template > $HADOOP_CONF_DIR/core-site.xml
 
 # setting spark defaults
-echo spark.yarn.jar hdfs:///spark/spark-assembly-1.6.0-hadoop2.6.0.jar > $SPARK_HOME/conf/spark-defaults.conf
-cp $SPARK_HOME/conf/metrics.properties.template $SPARK_HOME/conf/metrics.properties
+# cp $SPARK_HOME/conf/metrics.properties.template $SPARK_HOME/conf/metrics.properties
 
-service sshd start
 # Skip in standalone mode:
 #$HADOOP_HOME/sbin/start-dfs.sh
 #$HADOOP_HOME/sbin/start-yarn.sh
 
-if [[ ! -z $AWS_ACCESS_KEY_ID ]];
+if [[ ! -z $AWS_ACCESS_KEY_ID ]]; then
 	bash $SPARK_HOME/install_creds.sh
 fi
 
 CMD=${1:-"exit 0"}
-if [[ "$CMD" == testall ]];
+if [[ "$CMD" == testall ]]; then
 	bash $SPARK_HOME/testall.sh
 elif [[ "$CMD" == "-d" ]];
 then
-	service sshd stop
-	/usr/sbin/sshd -D -d
+	echo "sshd service not yet supported"
+	exit 1
+	#service sshd start
+	#service sshd stop
+	#/usr/sbin/sshd -D -d
 else
 	/bin/bash -c "$*"
 fi
