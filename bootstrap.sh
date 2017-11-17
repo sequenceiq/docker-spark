826cb837a7cb13cdce2dfd84bffeeff886bec8b3
Pondpaun7z <pppangpondpp7@gmail.com>
2017-11-17 22:37:12 +0700

    Added config

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,14 +12,11 @@ cd $HADOOP_PREFIX/share/hadoop/common ; for cp in ${ACP//,/ }; do  echo == $cp; 
 # altering the core-site configuration
 sed s/HOSTNAME/$HOSTNAME/ /usr/local/hadoop/etc/hadoop/core-site.xml.template > /usr/local/hadoop/etc/hadoop/core-site.xml
 
-# setting spark defaults
-echo spark.yarn.jar hdfs:///spark/spark-yarn_2.11-2.0.0.jar > $SPARK_HOME/conf/spark-defaults.conf
-cp $SPARK_HOME/conf/metrics.properties.template $SPARK_HOME/conf/metrics.properties
 
 service sshd start
 $HADOOP_PREFIX/sbin/start-dfs.sh
 $HADOOP_PREFIX/sbin/start-yarn.sh
-
+$HADOOP_PREFIX/sbin/mr-jobhistory-daemon.sh start historyserver
 
 
 CMD=${1:-"exit 0"}
